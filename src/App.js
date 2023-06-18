f1abff74d0a32a7afc36f99b5696e32873a2fe14
Gabriel Costa Soares <gsoares0404@gmail.com>
2023-06-18 22:21:08 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import './Fonts.css'
+
+
 
 function App() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [contact, setContact] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+    const data = {
+      name: name,
+      email: email,
+      contact: contact
+    };
+
+
+    fetch('url.com/submit', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+      .then(response => {
+        console.log('Form submitted successfully!');
+        setName('');
+        setEmail('');
+        setContact('');
+      })
+      .catch(error => {
+        console.error('Error submitting the form:', error);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <div className="form-container">
+        <h2>SEND ME A 'HELLO'</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="name">Name:</label>
+            <input
+              className='name-email'
+              type="text"
+              id="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="email">Email:</label>
+            <input
+              className='name-email'
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="contact" class='contact-box'>Contact:</label>
+            <textarea
+              type="tel"
+              id="contact"
+              className='contact'
+              value={contact}
+              onChange={(e) => setContact(e.target.value)}
+              required
+            />
+          </div>
+          <button type="submit">SEND</button>
+        </form>
+      </div>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,48 @@
-import { render, screen } from '@testing-library/react';
+import React, { useState } from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders form inputs and submit button', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const nameInput = screen.getByLabelText('Name:');
+  const emailInput = screen.getByLabelText('Email:');
+  const contactInput = screen.getByLabelText('Contact:');
+  const submitButton = screen.getByText('SEND');
+
+  expect(nameInput).toBeInTheDocument();
+  expect(emailInput).toBeInTheDocument();
+  expect(contactInput).toBeInTheDocument();
+  expect(submitButton).toBeInTheDocument();
+});
+
+test('allows user to type in form inputs', () => {
+  render(<App />);
+
+  const nameInput = screen.getByLabelText('Name:');
+  const emailInput = screen.getByLabelText('Email:');
+  const contactInput = screen.getByLabelText('Contact:');
+
+  fireEvent.change(nameInput, { target: { value: 'John Lennon' } });
+  fireEvent.change(emailInput, { target: { value: 'john@example.com' } });
+  fireEvent.change(contactInput, { target: { value: '1234567890' } });
+
+  expect(nameInput.value).toBe('John Lennon');
+  expect(emailInput.value).toBe('john@example.com');
+  expect(contactInput.value).toBe('1234567890');
+});
+
+test('submits form data when the submit button is clicked', () => {
+  render(<App />);
+
+  const nameInput = screen.getByLabelText('Name:');
+  const emailInput = screen.getByLabelText('Email:');
+  const contactInput = screen.getByLabelText('Contact:');
+  const submitButton = screen.getByText('SEND');
+
+  fireEvent.change(nameInput, { target: { value: 'John Lennen' } });
+  fireEvent.change(emailInput, { target: { value: 'john@example.com' } });
+  fireEvent.change(contactInput, { target: { value: '1234567890' } });
+
+  fireEvent.click(submitButton);
 });
